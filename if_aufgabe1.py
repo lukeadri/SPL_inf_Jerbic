ac7642ac22c6b6cff8ba0377bb65c3c39bdf6c8d
DichterDev <david.dr.ruescher@outlook.com>
2022-10-24 08:39:30 +0200

    new

---
+++ if_aufgabe1.py
@@ -0,0 +1,12 @@
+import random
+
+#Erstelle eine Zufallszahl zwischen 0 und 100
+
+	
+    print(random.randint(0,100))
+		
+#Gib die Zufallszahl aus
+		
+#Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+#Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
+#Wenn die Zahl größer als 50 ist gib aus "Large
